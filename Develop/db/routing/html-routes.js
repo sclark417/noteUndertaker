aa57de18f28ef6e8f49945e3aeab9260697d86d5
Stephen Clark <clark7382@hotmail.com>
2021-02-23 18:01:12 -0700

    fixing codes

--- Develop/db/routing/html-routes.js
+++ Develop/db/routing/html-routes.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = function (app) {
   app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../index.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
   });
 
   app.get("/notes", function (req, res) {
